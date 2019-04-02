7795b2431baf19d7292d4d5de5dd98a4ffaabaaf
Nithya Babu <nbabu@ztech.io>
2019-04-02 15:29:57 +0530

    image - annotation initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Annotation from 'react-image-annotation';
 
-class App extends Component {
-  render() {
+export default class App extends Component {
+  state = {
+    annotations: [],
+    annotation: {}
+  }
+ 
+  onChange = (annotation) => {
+    this.setState({ annotation })
+  }
+ 
+  onSubmit = (annotation) => {
+    const { geometry, data } = annotation
+ 
+    this.setState({
+      annotation: {},
+      annotations: this.state.annotations.concat({
+        geometry,
+        data: {
+          ...data,
+          id: Math.random()
+        }
+      })
+    })
+  }
+ 
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+        <Annotation
+          src='https://wi-images.condecdn.net/image/d91Wlky6Pw3/crop/810/f/googleandroidfine.jpg'
+          alt='Two pebbles anthropomorphized holding hands'
+ 
+          annotations={this.state.annotations}
+ 
+          type={this.state.type}
+          value={this.state.annotation}
+          onChange={this.onChange}
+          onSubmit={this.onSubmit}
+        />
+    )
   }
 }
-
-export default App;
